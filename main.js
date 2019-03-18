7f0c34f6e0ad8f24969262f3a155d823b9be9821
Mark Okafor <okaformark@gmail.com>
2019-03-18 08:13:52 -0500

    adding button event and clear function

--- main.js
+++ main.js
@@ -2,15 +2,21 @@ const printToDom = (divId, textToPrint) => {
     const selectedDiv = document.getElementById(divId);
     selectedDiv.innerHTML = textToPrint;
 };
+const hi = console.log('hi');
+const clear = (e) => {
+    const buttonId = e.target.id;
+    if (buttonId === 'tempInput' || 'tempOutput'){
+        //clear something
+    }
+};
 
 
-
-
-
+const buttonEvent = () => {
+    document.getElementById('clearBtn').addEventListener('click', clear);
+};
 
 const init =()=>{
-
+    buttonEvent();
 };
-  
-  
+
 init();
