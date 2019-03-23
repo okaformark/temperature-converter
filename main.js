fa35598fb2cd0123b4852482ae8886f0afba55fb
Mark Okafor <okaformark@gmail.com>
2019-03-23 10:20:08 -0500

    convert button work

--- main.js
+++ main.js
@@ -2,32 +2,42 @@ const printToDom = (divId, textToPrint) => {
     const selectedDiv = document.getElementById(divId);
     selectedDiv.innerHTML = textToPrint;
 };
-const hi = console.log('hi');
 
 const clear = () => {
     document.getElementById('tempInput').value='';
+    document.getElementById('tempOutput').value='';
 };
 
-const determineCoverter = (e) => {
-    const temp_input= document.getElementById('tempInput').value;
-    const buttonId = e.target.id;
 
-    console.log(document.getElementById('tempInput').value);
+const toCelsuis =() =>{
+    let temp_input= document.getElementById('tempInput').value;
+    const celsius = Math.round((temp_input - 32) * 5/9);
+    document.getElementById('tempOutput').value = celsius;
+    console.log(celsius);
 };
-// if (buttonId === 'celsius'){
+const toFahrenheit = () =>{
+    let temp_input= document.getElementById('tempInput').value;
+    const fahrenheit = Math.round((temp_input * 9/5) + 32); 
+    document.getElementById('tempOutput').value = fahrenheit;
+    console.log(fahrenheit);
+};
+
+const determineCoverter = () => {
     
-// }
-// else {
-//     const fahrenheit = (temp_input * 9/5) + 32; 
-//     document.getElementById('tempOutput').value = fahrenheit;
-//     console.log(fahrenheit);
-// }
+
+    if (document.getElementById('celsuis').checked){
+        toCelsuis();
+    }
+    else  if (document.getElementById('fahrenheit').checked) {
+        toFahrenheit();
+    }
+};
 
 
 
 const buttonEvent = () => {
-    document.getElementById('celsuis').addEventListener('click', determineCoverter);
-    document.getElementById('fahrenheit').addEventListener('click', determineCoverter);
+    document.getElementById('celsuis').checked = false;
+    document.getElementById('fahrenheit').checked = false;
     document.getElementById('convertBtn').addEventListener('click', determineCoverter);
     document.getElementById('clearBtn').addEventListener('click', clear);
 };
